123d36a34fda7cd7d7544b97cc91499c28b1df7b
Rendy-Zulianda-UAD <155240352+Rendy-Zulianda-UAD@users.noreply.github.com>
2023-12-31 23:42:32 +0700

    Update KonverterApp.java

--- src/aplikasi/KonverterApp.java
+++ src/aplikasi/KonverterApp.java
@@ -15,44 +15,93 @@ public class KonverterApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Contoh penggunaan konverter panjang
+        // Membuat objek konverter untuk panjang, berat, dan suhu
         Panjang panjangConverter = new PanjangImplementasi();
-        double panjangInput = getInput(scanner, "Masukkan panjang dalam meter: ");
-        System.out.println(panjangInput + " meter = " + panjangConverter.meterToKilometer(panjangInput) + " kilometer");
-        System.out.println(panjangInput + " meter = " + panjangConverter.meterToMil(panjangInput) + " mil");
-        System.out.println(panjangInput + " kilometer = " + panjangConverter.kilometerToMeter(panjangInput) + " meter");
-        System.out.println(panjangInput + " mil = " + panjangConverter.milToMeter(panjangInput) + " meter");
-        System.out.println(panjangInput + " kilometer = " + panjangConverter.kilometerToMil(panjangInput) + " mil");
-        System.out.println(panjangInput + " mil = " + panjangConverter.milToKilometer(panjangInput) + " kilometer");
-
-        // Contoh penggunaan konverter berat
         Berat beratConverter = new BeratImplementasi();
-        double beratInput = getInput(scanner, "Masukkan berat dalam gram: ");
-        System.out.println(beratInput + " gram = " + beratConverter.gramToKilogram(beratInput) + " kilogram");
-        System.out.println(beratInput + " gram = " + beratConverter.gramToPound(beratInput) + " pound");
-        System.out.println(beratInput + " kilogram = " + beratConverter.kilogramToGram(beratInput) + " gram");
-        System.out.println(beratInput + " pound = " + beratConverter.poundToGram(beratInput) + " gram");
-        System.out.println(beratInput + " kilogram = " + beratConverter.kilogramToPound(beratInput) + " pound");
-        System.out.println(beratInput + " pound = " + beratConverter.poundToKilogram(beratInput) + " kilogram");
-
-        // Contoh penggunaan konverter suhu
         Suhu suhuConverter = new SuhuImplementasi();
-        double suhuInput = getInput(scanner, "Masukkan suhu dalam Celsius: ");
-        System.out.println(suhuInput + " Celsius = " + suhuConverter.celsiusToFahrenheit(suhuInput) + " Fahrenheit");
-        System.out.println(suhuInput + " Fahrenheit = " + suhuConverter.fahrenheitToCelsius(suhuInput) + " Celsius");
-        System.out.println(suhuInput + " Kelvin = " + suhuConverter.kelvinToCelsius(suhuInput) + " Celsius");
-        System.out.println(suhuInput + " Celsius = " + suhuConverter.celsiusToKelvin(suhuInput) + " Kelvin");
 
+        // Menampilkan menu utama
+        int choice;
+        do {
+            System.out.println("=========== Konverter Unit ===========");
+            System.out.println("1. Konversi Panjang");
+            System.out.println("2. Konversi Berat");
+            System.out.println("3. Konversi Suhu");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih jenis konversi (0-3): ");
+
+            try {
+                choice = scanner.nextInt();
+                scanner.nextLine(); // Consume newline character
+                processConversion(scanner, choice, panjangConverter, beratConverter, suhuConverter);
+            } catch (InputMismatchException e) {
+                System.out.println("Input tidak valid. Harap masukkan angka.");
+                scanner.nextLine(); // Clear the buffer
+                choice = -1;
+            }
+
+        } while (choice != 0);
+
+        System.out.println("Terima kasih! Program selesai.");
         scanner.close();
     }
 
-    private static double getInput(Scanner scanner, String prompt) {
+    private static void processConversion(Scanner scanner, int choice, Panjang panjangConverter, Berat beratConverter, Suhu suhuConverter) {
+        switch (choice) {
+            case 1:
+                // Konversi Panjang
+                processPanjangConversion(scanner, panjangConverter);
+                break;
+            case 2:
+                // Konversi Berat
+                processBeratConversion(scanner, beratConverter);
+                break;
+            case 3:
+                // Konversi Suhu
+                processSuhuConversion(scanner, suhuConverter);
+                break;
+            case 0:
+                // Keluar
+                break;
+            default:
+                System.out.println("Pilihan tidak valid. Silakan pilih lagi.");
+                break;
+        }
+    }
+
+    private static void processPanjangConversion(Scanner scanner, Panjang panjangConverter) {
+        double input;
+        System.out.print("Masukkan panjang dalam meter: ");
+        input = getInput(scanner);
+        System.out.println(input + " meter = " + panjangConverter.meterToKilometer(input) + " kilometer");
+        System.out.println(input + " meter = " + panjangConverter.meterToMil(input) + " mil");
+        // Tambahkan konversi panjang lainnya jika diperlukan
+    }
+
+    private static void processBeratConversion(Scanner scanner, Berat beratConverter) {
+        double input;
+        System.out.print("Masukkan berat dalam gram: ");
+        input = getInput(scanner);
+        System.out.println(input + " gram = " + beratConverter.gramToKilogram(input) + " kilogram");
+        System.out.println(input + " gram = " + beratConverter.gramToPound(input) + " pound");
+        // Tambahkan konversi berat lainnya jika diperlukan
+    }
+
+    private static void processSuhuConversion(Scanner scanner, Suhu suhuConverter) {
+        double input;
+        System.out.print("Masukkan suhu dalam Celsius: ");
+        input = getInput(scanner);
+        System.out.println(input + " Celsius = " + suhuConverter.celsiusToFahrenheit(input) + " Fahrenheit");
+        System.out.println(input + " Fahrenheit = " + suhuConverter.fahrenheitToCelsius(input) + " Celsius");
+        // Tambahkan konversi suhu lainnya jika diperlukan
+    }
+
+    private static double getInput(Scanner scanner) {
         double input = 0;
         boolean validInput = false;
 
         while (!validInput) {
             try {
-                System.out.print(prompt);
                 input = scanner.nextDouble();
                 validInput = true;
             } catch (InputMismatchException e) {
